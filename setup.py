ace3743072de06c86adb9fffe6c01c48c0bcd825
Joel Becker <mail@joelbecker.ca>
2017-06-21 13:27:42 -0400

    Added session to parse_requirements in setup.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ version_string = '0.2.5'
 
 # Parse requirements
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./requirements.txt')
+install_reqs = parse_requirements('./requirements.txt', session=pip.download.PipSession())
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
