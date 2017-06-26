e2b69cdcd538bbcc116558e8ff47517ce2b6acb0
Jillian Anderson <jillianderson8@gmail.com>
2017-06-26 09:38:55 -0400

    Added GNU GPL v3 licensing header to the package's python files

--- tidyextractors/tidygit/get_log.py
+++ tidyextractors/tidygit/get_log.py
@@ -1,3 +1,22 @@
+# *********************************************************************************************
+# Copyright (C) 2017 Joel Becker,  Jillian Anderson, Steve McColl and Dr. John McLevey
+#
+# This file is part of the tidyextractors package developed for Dr John McLevey's Networks Lab
+# at the University of Waterloo. For more information, see
+# http://tidyextractors.readthedocs.io/en/latest/
+#
+# tidyextractors is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# tidyextractors is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with tidyextractors.
+# If not, see <http://www.gnu.org/licenses/>.
+# *********************************************************************************************
+
 import git
 import tqdm
 import pandas as pd
