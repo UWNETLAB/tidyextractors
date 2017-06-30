39ea3a9e7f747b7ccf221262dcfb181db4c2dcad
Joel Becker <mail@joelbecker.ca>
2017-06-30 13:48:23 -0400

    Tweaked parse requirements path.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ version_string = '0.3.2'
 
 # Parse requirements
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./requirements.txt', session=pip.download.PipSession())
+install_reqs = parse_requirements('requirements.txt', session=pip.download.PipSession())
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
